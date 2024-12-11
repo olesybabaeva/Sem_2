f87a717e4c58212316b8c82b27d7e49b5a676254
Lesya <olesybabaeva@gmail.com>
2024-12-11 13:57:40 +0300

    задания
    добавлены

---
+++ Zad_2.py
@@ -0,0 +1,27 @@
+﻿#Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
+# Программа должна возвращать сумму и произведение дробей. 
+# Для проверки своего кода используйте модуль fractions
+
+import fractions
+
+first: str = str(input("Введите числитель первой дроби: "))
+second: str = str(input("Введите знаменатель первой дроби: "))
+third: str = str(input("Введите числитель второй дроби: "))
+fourth: str = str(input("Введите знаменатель второй дроби: "))
+
+number1: int = int(first)
+number2: int = int(second)
+number3: int = int(third)
+number4: int = int(fourth)
+result1 = (number1 / number2 ) + (number3 / number4)
+result2 = (number1 / number2 ) * (number3 / number4)
+print(result1)
+print(result2)
+
+
+firstfraction = fractions.Fraction(number1, number2)
+secondfraction = fractions.Fraction(number3, number4)
+result3 = firstfraction + secondfraction
+result4 = firstfraction * secondfraction
+print(result3)
+print(result4)
